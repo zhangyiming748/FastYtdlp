2bc67bc5b7b69b1d6a20ec547e98c5284fc43fed
zen <zhangyiming748@gmail.com>
2025-10-02 14:32:27 +0800

    主要函数名改为Download 分辨视频来源

--- ytdlp.go
+++ ytdlp.go
@@ -6,23 +6,52 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/zhangyiming748/FastYtdlp/storage"
 	"github.com/zhangyiming748/FastYtdlp/util"
 	"github.com/zhangyiming748/FastYtdlp/ytdlp"
 )
 
-func Ytdlp(root, proxy string) {
+func Download(root string, yc YtdlpConfig) {
+	storage.SetMysql(yc.User, yc.Password, yc.Host, yc.Port)
+	storage.GetMysql().Sync2(storage.Pornhub{})
 	os.MkdirAll(root, os.ModePerm)
 	post := filepath.Join(root, "post.link")
+	log.Printf("根据%v文件开始下载\n", post)
 	lines := util.ReadByLine(post)
+
 	for i, line := range lines {
 		log.Printf("正在处理第%d个链接:%s", i+1, line)
-		if strings.Contains(line, "#") {
-			prefix := strings.Split(line, "#")[0]
-			suffix := strings.Split(line, "#")[1]
-			local := filepath.Join(root, suffix)
-			ytdlp.DownloadVideo(prefix, proxy, local)
+
+		if strings.Contains(line, "pornhub") {
+			var key string
+			if strings.Contains(line, "#") {
+				prefix := strings.Split(line, "#")[0]
+				key = strings.Split(prefix, "=")[1]
+				suffix := strings.Split(line, "#")[1]
+				local := filepath.Join(root, suffix)
+				name := ytdlp.DownloadVideo(prefix, yc.Proxy, local)
+				one := new(storage.Pornhub)
+				one.Key = key
+				if has, _ := one.FindByKey(); has {
+					continue
+				}
+				one.Name = name
+				one.From = "pornhub"
+				one.InsertOne()
+			} else {
+				name := ytdlp.DownloadVideo(line, yc.Proxy, root)
+				key = strings.Split(line, "=")[1]
+				one := new(storage.Pornhub)
+				one.Key = key
+				if has, _ := one.FindByKey(); has {
+					continue
+				}
+				one.Name = name
+				one.From = "pornhub"
+				one.InsertOne()
+			}
 		} else {
-			ytdlp.DownloadVideo(line, proxy, root)
+			//其他来源的网站
 		}
 	}
 }
--- ytdlp_test.go
+++ ytdlp_test.go
@@ -6,6 +6,11 @@ import (
 
 func TestYtdlp(t *testing.T) {
 	root := "C:\\Users\\zen\\Github\\FastYtdlp"
-	proxy := "192.168.5.2:8889"
-	Ytdlp(root, proxy)
+	var yc YtdlpConfig
+	yc.Host = "192.168.5.2"
+	yc.Port = "3306"
+	yc.User = "root"
+	yc.Password = "163453"
+	yc.Proxy = "192.168.5.2:8889"
+	Download(root, yc)
 }
