5a7424f0384ff0de5df6432614a9c0e62b19e452
zen <zhangyiming748@gmail.com>
2025-10-03 22:51:56 +0800

    数据库表名改为video

--- storage/model.go
+++ storage/model.go
@@ -4,20 +4,24 @@ import (
 	"time"
 )
 
-type Pornhub struct {
+type Video struct {
 	Id        int64     `xorm:"pk autoincr notnull comment('主键id') INT(11)"`
-	Key       string    `xorm:"varchar(255) comment(下载视频的唯一标识)"`
-	Name      string    `xorm:"varchar(255) comment(视频名称)"`
+	Keyword   string    `xorm:"varchar(255) comment(下载视频的唯一标识)"`
+	Name      string    `xorm:"TEXT(255) comment(视频名称)"`
 	From      string    `xorm:"varchar(255) comment(视频来源)"`
+	Url       string `xorm:"varchar(255) comment(视频链接)"`
 	CreatedAt time.Time `xorm:"created comment(创建时间)"`
 	UpdatedAt time.Time `xorm:"updated comment(修改时间)"`
 	DeletedAt time.Time `xorm:"deleted comment(删除时间)"`
 }
 
-func (p *Pornhub) InsertOne() (int64, error) {
-	return GetMysql().InsertOne(p)
+func (v *Video) InsertOne() (int64, error) {
+	return GetMysql().InsertOne(v)
 }
 
-func (p *Pornhub) FindByKey() (bool, error) {
-	return GetMysql().Where("`key` = ?", p.Key).Get(p)
+func (v *Video) FindByKeyword() (bool, error) {
+	return GetMysql().Where("keyword = ?", v.Keyword).Get(v)
+}
+func (v *Video) FindByUrl() (bool, error) {
+	return GetMysql().Where("uri = ?", v.Url).Get(v)
 }
