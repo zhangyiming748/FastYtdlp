f93775279800f9c321cf73fd7aba0c3d7e37b161
surface pro 9 <zhangyiming748@gmail.com>
2025-10-03 23:41:18 +0800

    判断重复的逻辑移动到下载之前

--- ytdlp.go
+++ ytdlp.go
@@ -2,72 +2,76 @@ package FastYtdlp
 
 import (
 	"log"
-	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
 	"github.com/zhangyiming748/FastYtdlp/storage"
 	"github.com/zhangyiming748/FastYtdlp/util"
-	"github.com/zhangyiming748/FastYtdlp/ytdlp"
 )
 
 func Download(root string, yc YtdlpConfig) {
 	storage.SetMysql(yc.User, yc.Password, yc.Host, yc.Port)
-	storage.GetMysql().Sync2(storage.Video{})
-	os.MkdirAll(root, os.ModePerm)
+	err := storage.GetMysql().Sync2(storage.Video{})
+	if err != nil {
+		log.Fatalf("创建数据库同步表结构连接失败:%v\n", err)
+	}
 	post := filepath.Join(root, "post.link")
 	log.Printf("根据%v文件开始下载\n", post)
 	lines := util.ReadByLine(post)
-
 	for i, line := range lines {
 		log.Printf("正在处理第%d个链接:%s", i+1, line)
-
-		if strings.Contains(line, "pornhub") {
-			var key string
-			if strings.Contains(line, "#") {
-				uri := strings.Split(line, "#")[0]
-				key = strings.Split(uri, "=")[1]
-				subFolder := strings.Split(line, "#")[1]
-				local := filepath.Join(root, subFolder)
-				name := ytdlp.DownloadVideo(uri, yc.Proxy, local)
-				one := new(storage.Video)
-				one.Keyword = key
-				if has, err := one.FindByKeyword(); err != nil {
-					log.Fatalf("查询数据库失败:%v\n", err)
-				} else if has {
-					log.Printf("由于数据库中已存在%v\t跳过此次下载\n", one.Name)
-					continue
-				}
-				one.Name = name
-				one.From = "pornhub"
-				insertOne, err := one.InsertOne()
-				if err != nil {
-					log.Printf("插入%d条数据失败:%v\n", insertOne, err)
-				} else {
-					log.Printf("成功插入%d条数据\n", insertOne)
-				}
-			} else {
-				name := ytdlp.DownloadVideo(line, yc.Proxy, root)
-				key = strings.Split(line, "=")[1]
-				one := new(storage.Video)
-				one.Keyword = key
-				if has, err := one.FindByKeyword(); err != nil {
-					log.Fatalf("查询数据库失败:%v\n", err)
-				} else if has {
-					log.Printf("由于数据库中已存在%v\t跳过此次下载\n", one.Name)
-					continue
-				}
-				one.Name = name
-				one.From = "pornhub"
-				insertOne, err := one.InsertOne()
-				if err != nil {
-					log.Printf("插入%d条数据失败:%v\n", insertOne, err)
-				} else {
-					log.Printf("成功插入%d条数据\n", insertOne)
-				}
-			}
+		var name string
+		if strings.Contains(line, "#") {
+			uri := strings.Split(line, "#")[0]
+			hashTag := strings.Split(line, "#")[1]
+			local := filepath.Join(root, hashTag)
+			name = DownloadHelper(uri, yc.Proxy, local)
 		} else {
-			//其他来源的网站
+			name = DownloadHelper(line, yc.Proxy, root)
 		}
+		log.Printf("下载%v\n流程结束", name)
 	}
+}
+func DownloadHelper(uri, proxy, location string) (title string) {
+	if has, err := sameUrl(uri); err != nil {
+		log.Fatalf("查询数据库失败:%v\n", err)
+	} else if has {
+		log.Printf("由于数据库中已存在相同链接%v\t跳过此次下载\n", uri)
+		return uri
+	}
+	nameCmd := exec.Command("yt-dlp", "--proxy", proxy, "-f", "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/mp4", "--no-playlist", "--paths", location, "--get-filename", uri)
+	name := util.GetVideoName(nameCmd)
+	name = filepath.Base(name)
+	if has, err := sameName(uri); err != nil {
+		log.Fatalf("查询数据库失败:%v\n", err)
+	} else if has {
+		log.Printf("由于数据库中已存在同名文件%v\t跳过此次下载\n", name)
+		return uri
+	}
+	log.Printf("当前下载的文件标题:%s", name)
+	downloadCmd := exec.Command("yt-dlp", "--proxy", proxy, "-f", "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/mp4", "--no-playlist", "--paths", location, uri)
+	util.ExecCommand4Ytdlp(downloadCmd)
+	log.Printf("当前下载成功的文件标题:%s", name)
+	one := new(storage.Video)
+	one.Url = uri
+	one.Name = name
+	insertOne, err := one.InsertOne()
+	if err != nil {
+		log.Fatalf("插入%d条数据失败:%v\n", insertOne, err)
+	} else {
+		log.Printf("成功插入%d条数据\n", insertOne)
+	}
+	return name
+}
+
+func sameUrl(uri string) (bool, error) {
+	one := new(storage.Video)
+	one.Url = uri
+	return one.FindByUrl()
+}
+func sameName(name string) (bool, error) {
+	one := new(storage.Video)
+	one.Name = name
+	return one.FindByName()
 }
